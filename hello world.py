45c4f6f7918f265f42139df1d8b1bd34f6867ced
AlexanderdpS <adominguez@senati.pe>
2024-08-21 11:53:28 -0500

    Correcion de hola mundo

--- hello world.py
+++ hello world.py
@@ -1,7 +1,1 @@
-Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print "hello world"
->>> [DEBUG ON]
->>> [DEBUG OFF]
->>> print "hello world"
-SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print ("Hola mundo")
